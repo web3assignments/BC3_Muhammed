52fcddf12b65e979578c1ce3845fec944b3017a7
msan41 <74299282+msan41@users.noreply.github.com>
2021-01-07 17:03:19 +0100

    Update index.js

--- PD-05/web3/index.js
+++ PD-05/web3/index.js
@@ -130,3 +130,14 @@ async function send() {
     console.log(result.events)
     console.log(result.events.Purchase.returnValues)
 }
+
+Coin.Sent().watch({}, '', function(error, result) {
+    if (!error) {
+        console.log("Coin transfer: " + result.args.amount +
+            " coins were sent from " + result.args.from +
+            " to " + result.args.to + ".");
+        console.log("Balances now:\n" +
+            "Sender: " + Coin.balances.call(result.args.from) +
+            "Receiver: " + Coin.balances.call(result.args.to));
+    }
+})
