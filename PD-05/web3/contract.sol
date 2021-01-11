eb8e81dcda89b8aa8a8a34bcee4dd21bc1ee5746
Gerard Persoon <mail@gpersoon.com>
2021-01-11 10:27:36 +0100

    Create contract.sol

---
+++ PD-05/web3/contract.sol
@@ -0,0 +1,28 @@
+pragma solidity >=0.4.0 <0.7.0;
+
+contract Coin {
+    address public user;
+    mapping (address => uint) public balances;
+
+
+    constructor() public {
+        user = msg.sender;
+        balances[msg.sender] = 10000000;
+    }
+    
+    event Sent(address from, address to, uint amount);
+    event Received(address from, uint amount);
+    
+
+    function send(address receiver, uint amount) public {
+        require(amount <= balances[msg.sender], "Insufficient balance.");
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        emit Sent(msg.sender, receiver, amount);
+    }
+    
+    function Receive() external payable {
+        emit Received(msg.sender, msg.value);
+    }
+
+}
